c268e48ebfb8c2fa05fcbcb709bdc5ee5201e182
danielszuk <daniel.szuk@futuristic.digital>
2024-02-05 14:37:02 +0100

    feat: cache queries by default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
